315767c8e6a18d0e86ecdb331f95823cc6c8a0d3
vicrodh <103399283+vicrodh@users.noreply.github.com>
2025-12-10 14:00:45 -0600

    fix: add alias for  to resolve pdfjs util

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,12 @@ import { svelte } from '@sveltejs/vite-plugin-svelte';
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [svelte()],
+  resolve: {
+    alias: {
+      $lib: '/src/lib',
+      $types: '/src/types',
+    },
+  },
   clearScreen: false,
   server: {
     port: 5173,
